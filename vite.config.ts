e9425da05f8c6f5f14d6ac04881a89e15abf5c2f
Juan Antonio <juanantonio.cx@gmail.com>
2022-12-05 00:12:38 -0500

    feat: add eslint + prettier support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      {find: "@components",replacement: path.resolve(__dirname, "src/components")},
+      {find: "@setup",replacement: path.resolve(__dirname, "src/setup")},
+    ],
+  },
 });
